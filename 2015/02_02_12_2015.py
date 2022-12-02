bf60a094ce5c682e7eb17830d7f60e0eb9eeaa0f
Matthias Schulte <matthias.schulte@adesso-insurance-solutions.de>
2022-12-02 11:17:07 +0100

    2015 - Problem 2, Teil 2, fertig

--- 2015/02_02_12_2015.py
+++ 2015/02_02_12_2015.py
@@ -12,6 +12,9 @@ class Present:
     def organize_measures_as_list(self) -> list[int]:
         return [self.width, self.length, self.height]
 
+    def calculate_volume(self) -> int:
+        return self.width * self.height * self.length
+
     def calculate_surface_area(self) -> int:
         width = self.width
         length = self.length
@@ -22,8 +25,15 @@ class Present:
         sorted_measures = self.sort_measures()
         return sorted_measures[0] * sorted_measures[1]
 
+    def calculate_ribbon_length(self) -> int:
+        sorted_measures = self.sort_measures()
+        return 2 * sorted_measures[0] + 2 * sorted_measures[1]
+
     def calculate_total_wrapping_area(self):
         return self.calculate_surface_area() + self.calculate_additional_wrapping_surface()
+
+    def calculate_total_ribbon_length(self):
+        return self.calculate_ribbon_length() + self.calculate_volume()
 
     def sort_measures(self) -> list[int]:
         sorted_measures = self.organize_measures_as_list()
@@ -31,11 +41,18 @@ class Present:
         return sorted_measures
 
 
-def calculate_total_wrapping_area_for_all_presents():
+def calculate_total_wrapping_area_for_all_presents() -> int:
     list_of_all_measures = ip.read_input_list("input_data/02_12_problem_data.txt")
     trimmed_list = ip.trim_newlines(list_of_all_measures)
     presents = unzip_measures_to_presents(trimmed_list)
     return calculate_total_wrapping_area(presents)
+
+
+def calculate_total_ribbon_length_for_all_presents() -> int:
+    list_of_all_measures = ip.read_input_list("input_data/02_12_problem_data.txt")
+    trimmed_list = ip.trim_newlines(list_of_all_measures)
+    presents = unzip_measures_to_presents(trimmed_list)
+    return calculate_total_ribbon_length(presents)
 
 
 def unzip_measures_to_presents(list_of_measures: list[str]) -> list[Present]:
@@ -64,9 +81,18 @@ def calculate_total_wrapping_area(presents: list[Present]) -> int:
     return wrapping_area
 
 
+def calculate_total_ribbon_length(presents: list[Present]) -> int:
+    ribbon_length = 0
+    for present in presents:
+        ribbon_length += present.calculate_total_ribbon_length()
+    return ribbon_length
+
+
 def main():
     wrapping_area = calculate_total_wrapping_area_for_all_presents()
     print(f'The elves should order {wrapping_area} square feet of wrapping paper.')
+    ribbon_length = calculate_total_ribbon_length_for_all_presents()
+    print(f'The elves should order {ribbon_length} feet of ribbon.')
     pass
 
 
