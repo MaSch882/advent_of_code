c8ea164bfe051f5155586ce609f9025e1880ba06
Matthias Schulte <matthias.schulte@adesso-insurance-solutions.de>
2022-12-05 11:09:34 +0100

    2022 - Problem 5, fertig

--- 2022/solutions/run_all.py
+++ 2022/solutions/run_all.py
@@ -2,6 +2,7 @@ import solution_01_12_2022 as sol_01
 import solution_02_12_2022 as sol_02
 import solution_03_12_2022 as sol_03
 import solution_04_12_2022 as sol_04
+import solution_05_12_2022 as sol_05
 
 
 def main():
@@ -9,6 +10,7 @@ def main():
     sol_02.main()
     sol_03.main()
     sol_04.main()
+    sol_05.main()
 
 
 if __name__ == "__main__":
--- 2022/solutions/solution_05_12_2022.py
+++ 2022/solutions/solution_05_12_2022.py
@@ -41,13 +41,15 @@ STACK_8 = ["S", "J", "F", "L", "H", "W", "Z", "Q"]
 STACK_9 = ["S", "Q", "P", "W", "N"]
 
 STACKS = [None, STACK_1, STACK_2, STACK_3, STACK_4, STACK_5, STACK_6, STACK_7, STACK_8, STACK_9]
+INITIAL_STACKS = [None, STACK_1.copy(), STACK_2.copy(), STACK_3.copy(), STACK_4.copy(), STACK_5.copy(), STACK_6.copy(),
+                  STACK_7.copy(), STACK_8.copy(), STACK_9.copy()]
 
 
 def remove_and_return_last_item(list_of_strings: list[str]) -> str:
     return list_of_strings.pop()
 
 
-def move_crates_from_stack_to_stack(number_of_crates: int, stack_start: int, stack_end: int):
+def move_crates_from_stack_to_stack(number_of_crates: int, stack_start: int, stack_end: int) -> None:
     stack_to_move_from = STACKS[stack_start]
     stack_to_move_to = STACKS[stack_end]
     for i in range(1, number_of_crates + 1):
@@ -63,7 +65,7 @@ def perform_crane_transitions(instructions: list[list[int]]) -> None:
         move_crates_from_stack_to_stack(number_of_crates, stack_start, stack_end)
 
 
-def calculate_message_at_the_end():
+def calculate_message_at_the_end() -> str:
     message_string = ""
     for stack in STACKS:
         if stack is None:
@@ -80,11 +82,60 @@ def perform_all_crane_instructions_and_calculate_message(filename: str) -> str:
     return message
 
 
+# Problem 2
+
+def remove_and_return_last_items(list_of_strings: list[str], number_of_elements: int) -> list[str]:
+    removed_elements = []
+    for i in range(1, number_of_elements + 1):
+        removed_elements.append(remove_and_return_last_item(list_of_strings))
+    removed_elements.reverse()
+    return removed_elements
+
+
+def move_crates_together_from_stack_to_stack(number_of_crates: int, stack_start: int, stack_end: int) -> None:
+    stack_to_move_from = STACKS[stack_start]
+    stack_to_move_to = STACKS[stack_end]
+    moved_items = remove_and_return_last_items(stack_to_move_from, number_of_crates)
+    for item in moved_items:
+        stack_to_move_to.append(item)
+
+
+def perform_crane_transitions_together(instructions: list[list[int]]) -> None:
+    for instruction in instructions:
+        number_of_crates = instruction[0]
+        stack_start = instruction[1]
+        stack_end = instruction[2]
+        move_crates_together_from_stack_to_stack(number_of_crates, stack_start, stack_end)
+
+
+def perform_all_crane_instructions_together_and_calculate_message(filename: str) -> str:
+    input_data = read_input(filename)
+    crane_instructions = transform_input_in_integers(input_data)
+    perform_crane_transitions_together(crane_instructions)
+    message = calculate_message_at_the_end()
+    return message
+
+
+def revert_changes_on_stacks() -> None:
+    for i, stack in enumerate(STACKS):
+        stack = INITIAL_STACKS[i]
+        STACKS[i] = stack
+
+
 def main():
     print("Solutions to problem 5: [https://adventofcode.com/2022/day/5]")
 
     message = perform_all_crane_instructions_and_calculate_message("../input_data/05_12_problem_data.txt")
     print(f'The message delivered after all crane instructions are performed is {message}.')
+
+    revert_changes_on_stacks()
+
+    message_with_new_crane = perform_all_crane_instructions_together_and_calculate_message(
+        "../input_data/05_12_problem_data.txt")
+    print(
+        f'The message delivered after all crane instructions are performed on the new model is {message_with_new_crane}.')
+
+    print("")
 
 
 if __name__ == "__main__":
