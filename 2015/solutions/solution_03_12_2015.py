4a099e208f12ed18f399d73364588b602a5498e6
Matthias Schulte <matthias.schulte@adesso-insurance-solutions.de>
2022-12-06 17:03:45 +0100

    2015 - Problem 3, fertig

--- 2015/solutions/solution_03_12_2015.py
+++ 2015/solutions/solution_03_12_2015.py
@@ -3,6 +3,12 @@ from dataclasses import dataclass
 import Utils.input_processing as ip
 
 
+def read_input(filename: str) -> str:
+    list_of_strings = ip.read_input_list(filename)
+    list_of_strings = ip.trim_newlines(list_of_strings)
+    return list_of_strings[0]
+
+
 @dataclass()
 class Santa:
     pos_x: int
@@ -36,29 +42,56 @@ class Santa:
         if coordinates not in self.houses_visited:
             self.houses_visited.append(coordinates)
 
-    def execute_delivery_route(self, route: str):
-        for instruction in route:
-            self.performing_one_elf_instruction(instruction)
-            self.update_houses_visited()
 
-    def count_visited_houses(self):
-        return len(self.houses_visited)
+# Problem 1
 
 
-def read_input(filename: str) -> str:
-    list_of_strings = ip.read_input_list(filename)
-    list_of_strings = ip.trim_newlines(list_of_strings)
-    return list_of_strings[0]
+def execute_delivery_route(santa: Santa, route: str) -> None:
+    for instruction in route:
+        santa.performing_one_elf_instruction(instruction)
+        santa.update_houses_visited()
+
+
+def count_visited_houses(santa: Santa) -> int:
+    return len(santa.houses_visited)
+
+
+# Problem 2
+
+
+def execute_alternating_delivery_route(santa: Santa, robot_santa: Santa, route: str) -> None:
+    for i, instruction in enumerate(route):
+        if i % 2 == 0:
+            santa.performing_one_elf_instruction(instruction)
+            santa.update_houses_visited()
+        else:
+            robot_santa.performing_one_elf_instruction(instruction)
+            robot_santa.update_houses_visited()
+
+
+def count_combined_visited_houses(santa: Santa, robot_santa: Santa) -> int:
+    houses_visited_by_santa = santa.houses_visited
+    houses_visited_by_robot_santa = robot_santa.houses_visited
+    houses_visited = houses_visited_by_santa + houses_visited_by_robot_santa
+    unique_houses_visited = set(houses_visited)
+    return len(unique_houses_visited)
 
 
 def main():
     santa = Santa(0, 0, [(0, 0)])
     elves_instructions = read_input("../input_data/03_12_problem_data.txt")
 
-    santa.execute_delivery_route(elves_instructions)
-    number_of_houses_visited = santa.count_visited_houses()
-    print(f'The number of houses which receive at least one present is {number_of_houses_visited}.')
-    
+    execute_delivery_route(santa, elves_instructions)
+    number_of_houses_visited = count_visited_houses(santa)
+    print(f'The number of houses which receive at least one present from Santa is {number_of_houses_visited}.')
+
+    santa = Santa(0, 0, [(0, 0)])
+    robot_santa = Santa(0, 0, [(0, 0)])
+    execute_alternating_delivery_route(santa, robot_santa, elves_instructions)
+    number_of_unique_houses_visited = count_combined_visited_houses(santa, robot_santa)
+    print(
+        f'The number of houses which receive at least one present from Santa or Robot Santa is {number_of_unique_houses_visited}.')
+
     pass
 
 
