263f434490dc4b819980eb44967432703b321478
Matthias Schulte <matthias.schulte@adesso-insurance-solutions.de>
2023-06-26 10:36:45 +0200

    2016 - Problem 4
    RoomTester-Datenstruktur: Initialisierung und Sortierung

--- 2016/solutions/day_04/main_day_04_2016.py
+++ 2016/solutions/day_04/main_day_04_2016.py
@@ -1,4 +1,5 @@
 from structure_day_04_2016 import Room
+from structure_day_04_2016 import RoomTester
 
 filename = r"..\..\input_data\input_day_04_2016.txt"
 
@@ -7,6 +8,18 @@ testdata = ["aaaaa-bbb-z-y-x-123[abxyz]", "a-b-c-d-e-f-g-h-987[abcde]", "not-a-r
 
 
 def main():
+    room1 = Room(testdata[0])
+    room2 = Room(testdata[1])
+    room3 = Room(testdata[2])
+    room4 = Room(testdata[3])
+    rooms = [room1, room2, room3, room4]
+
+    for room in rooms:
+        print_room_data(room)
+
+    roomtester = RoomTester(room3)
+    print(roomtester.symbol_counter)
+
     print_solution_part_1()  #
     print_solution_part_2()  #
 
--- 2016/solutions/day_04/structure_day_04_2016.py
+++ 2016/solutions/day_04/structure_day_04_2016.py
@@ -1,47 +1,67 @@
 class Room:
-    full_name: str
     encrypted_name: str = ""
     sector_id: str = ""
     checksum: str = ""
 
     def __init__(self, full_name: str):
-        self.full_name = full_name
+        separated_parts_of_full_room_name = full_name.split("-")
+        combined_id_and_checksum = separated_parts_of_full_room_name.pop()
 
-        splitted = self.full_name.split("-")
-        sector_id_and_checksum = splitted.pop()
-
-        self.extract_encrypted_name(splitted)
-        self.extract_sector_id(sector_id_and_checksum)
-        self.extract_checksum(sector_id_and_checksum)
+        self.extract_encrypted_name(separated_parts_of_full_room_name)
+        self.extract_sector_id(combined_id_and_checksum)
+        self.extract_checksum(combined_id_and_checksum)
 
     def extract_encrypted_name(self, splitted_name: list[str]):
         for element in splitted_name:
             self.encrypted_name += str(element)
 
-    def extract_sector_id(self, id_and_checksum: str):
-        for char in id_and_checksum:
+    def extract_sector_id(self, combined_id_and_checksum: str):
+        for char in combined_id_and_checksum:
             if char != "[":
                 self.sector_id += str(char)
-                id_and_checksum = id_and_checksum[1:]
+                combined_id_and_checksum = combined_id_and_checksum[1:]
             else:
                 break
 
-    def extract_checksum(self, id_and_checksum: str):
-        for char in id_and_checksum:
+    def extract_checksum(self, combined_id_and_checksum: str):
+        # Alles bis zur '[' abschneiden
+        for char in combined_id_and_checksum:
             if char != "[":
-                id_and_checksum = id_and_checksum[1:]
+                combined_id_and_checksum = combined_id_and_checksum[1:]
             else:
                 break
 
-        for char in id_and_checksum:
+        # Alles in den Klammern an die Checksum antackern
+        for char in combined_id_and_checksum:
             if char == "[":
-                id_and_checksum = id_and_checksum[1:]
+                combined_id_and_checksum = combined_id_and_checksum[1:]
             elif char != "]":
                 self.checksum += str(char)
-                id_and_checksum = id_and_checksum[1:]
+                combined_id_and_checksum = combined_id_and_checksum[1:]
             else:
                 break
 
 
 class RoomTester:
-    symbol_counter: dict[str, int]
+    room_to_test: Room
+    symbol_counter: dict[str, int] = {}
+
+    def __init__(self, room: Room):
+        self.room_to_test = room
+        self.initialize_symbol_counter(room)
+        self.sort_symbols()
+
+    def initialize_symbol_counter(self, room):
+        encrypted_name = room.encrypted_name
+
+        for char in encrypted_name:
+            if char in self.symbol_counter:
+                self.symbol_counter[char] += 1
+            else:
+                self.symbol_counter[char] = 1
+
+    def sort_symbols(self):
+        symbols = self.symbol_counter
+        sorted_symbols = {key: value for key, value in
+                          sorted(symbols.items(), key=lambda symbol: symbol[1], reverse=True)}
+        self.symbol_counter = sorted_symbols
