cf9329d9a6676f45a1f227b63f4dece4643d1a8b
Matthias Schulte <matthias.schulte@adesso-insurance-solutions.de>
2023-06-26 09:29:32 +0200

    2016 - Problem 4
    Struktur angelegt

---
+++ 2016/solutions/day_04/main_day_04_2016.py
@@ -0,0 +1,21 @@
+filename = r"..\..\input_data\input_day_04_2016.txt"
+
+testdata = ["aaaaa-bbb-z-y-x-123[abxyz]", "a-b-c-d-e-f-g-h-987[abcde]", "not-a-real-room-404[oarel]",
+            "totally-real-room-200[decoy]"]
+
+
+def main():
+    print_solution_part_1()  #
+    print_solution_part_2()  #
+
+
+def print_solution_part_1():
+    pass
+
+
+def print_solution_part_2():
+    pass
+
+
+if __name__ == "__main__":
+    main()
