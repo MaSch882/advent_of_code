cbe4756dd43e6368709051e591d85dbb6ac801b6
Matthias Schulte <matthias.schulte@adesso-insurance-solutions.de>
2023-06-28 08:17:43 +0200

    2016 - Problem 5
    Teil 1 fertig.

--- 2016/solutions/day_05/main_day_05_2016.py
+++ 2016/solutions/day_05/main_day_05_2016.py
@@ -1,10 +1,16 @@
-from structure_day_05_2016 import MD5Hasher, MD5Checker
+from structure_day_05_2016 import InOrderPasswordCracker
 
 
 def main():
-    test_hash = MD5Hasher.hash("abc3231929")
-    print(test_hash)
-    print(MD5Checker.starts_with_five_zeroes(test_hash))
+    print_solution_part_1()
+
+
+def print_solution_part_1():
+    door_key = "reyedfim"
+    cracker = InOrderPasswordCracker(door_key, 8, 6)
+    password = cracker.crack_password()
+
+    print(f"The password for door {door_key} is {password}.")
 
 
 if __name__ == "__main__":
--- 2016/solutions/day_05/structure_day_05_2016.py
+++ 2016/solutions/day_05/structure_day_05_2016.py
@@ -17,3 +17,32 @@ class MD5Checker:
     @staticmethod
     def starts_with_five_zeroes(hashed: str) -> bool:
         return MD5Checker.starts_with_substring(hashed, "00000")
+
+
+class InOrderPasswordCracker:
+    door_key: str
+    password_length: int
+    interesting_bit: int
+    appendix: int
+    password: str
+
+    def __init__(self, door_key: str, password_length: int, interesting_bit: int):
+        self.door_key = door_key
+        self.password_length = password_length
+        self.interesting_bit = interesting_bit - 1
+        self.appendix = 1
+        self.password = ""
+
+    def crack_password(self) -> str:
+        for i in range(0, self.password_length):
+            next_hash_string = self.find_next_hash_string()
+            self.password += next_hash_string[5]
+        return self.password
+
+    def find_next_hash_string(self) -> str:
+        hashed_candidate = ""
+        while not MD5Checker.starts_with_five_zeroes(hashed_candidate):
+            candidate = self.door_key + str(self.appendix)
+            hashed_candidate = MD5Hasher.hash(candidate)
+            self.appendix += 1
+        return hashed_candidate
