627c1fb7024d1adc61297e26d0eb3c35a5c80db3
Matthias Schulte <matthias.schulte@adesso-insurance-solutions.de>
2023-07-03 09:34:30 +0200

    2016 - Problem 7
    Teil 1 fertig.

--- 2016/solutions/day_07/main_day_07_2016.py
+++ 2016/solutions/day_07/main_day_07_2016.py
@@ -1,59 +1,24 @@
+from Utils import input_processing
+from structure_day_07_2016 import TLSCounter
+
 filename = r"..\..\input_data\input_day_07_2016.txt"
+
+test_data = ["abba[mnop]qrst", "abcd[bddb]xyyx", "aaaa[qwer]tyui", "ioxxoj[asdfgh]zxcvbn", ]
 
 
 def main():
-    test_string_1 = "ioxxoj[asdfgh]zxcvbn"
-    test_string_2 = "abcd[bddb]xyyx"
-    test_string_3 = "aaaa[qwer]tyui"
+    ips = read_input()
+    print_solution_part_1(ips)
 
-    # Hypernet Sequences aufloesen
-    reframed = test_string_3.replace("]", "[")
-    splitted = reframed.split("[")
 
-    # Nur nicht HS betrachten
-    non_hypernet_sequences = []
-    hypernet_sequences = []
-    for i, x in enumerate(splitted):
-        if i % 2 == 0:
-            non_hypernet_sequences.append(x)
-        if i % 2 == 1:
-            hypernet_sequences.append(x)
+def print_solution_part_1(ips: list[str]):
+    counter = TLSCounter()
+    ips_with_tls = counter.count_ips_with_tls(ips)
+    print(f"{ips_with_tls} IPs support TLS.")
 
-    # Auf ABBA testen
-    has_abba_in_non_hypertext_sequence = False
-    has_abba_in_hypertext_sequence = False
 
-    for sequence in non_hypernet_sequences:
-        for i, char in enumerate(sequence):
-            try:
-                first = char
-                second = sequence[i + 1]
-                third = sequence[i + 2]
-                fourth = sequence[i + 3]
-
-                if first == fourth and second == third:
-                    has_abba_in_non_hypertext_sequence = True
-
-            except IndexError:
-                break
-
-    for sequence in hypernet_sequences:
-        for i, char in enumerate(sequence):
-            try:
-                first = char
-                second = sequence[i + 1]
-                third = sequence[i + 2]
-                fourth = sequence[i + 3]
-
-                if first == fourth and second == third:
-                    has_abba_in_hypertext_sequence = True
-
-            except IndexError:
-                break
-
-    supports_tls = has_abba_in_non_hypertext_sequence and not has_abba_in_hypertext_sequence
-
-    print(supports_tls)
+def read_input():
+    return input_processing.read_input(filename)
 
 
 if __name__ == "__main__":
--- 2016/solutions/day_07/structure_day_07_2016.py
+++ 2016/solutions/day_07/structure_day_07_2016.py
@@ -1,20 +1,75 @@
 class TLSChecker:
     ip_adress: str
+    hypernet_seq: list[str]
+    non_hypernet_seq: list[str]
 
     def __init__(self, ip: str):
         self.ip_adress = ip
+        self.hypernet_seq = []
+        self.non_hypernet_seq = []
+        self.has_palindrome_hypernet_seq = None
+        self.has_palindrome_non_hypernet_seq = None
 
     def supports_tls(self):
-        pass
+        self.extract_hypernet_sequences()
+        print(f"1: {self.hypernet_seq}")
+        print(f"2: {self.non_hypernet_seq}")
+        has_hypertext_palindrome = self.has_palindrome_in_hypernet_seq()
+        has_non_hypertext_palindrome = self.has_palindrome_in_non_hypernet_seq()
+        return has_non_hypertext_palindrome and not has_hypertext_palindrome
 
-    def extract_non_hypernet_sequences(self):
-        pass
+    def split_ip_up(self) -> list[str]:
+        return self.ip_adress.replace("]", "[").split("[")
 
     def extract_hypernet_sequences(self):
-        pass
+        splitted_ip = self.split_ip_up()
+        for index, block in enumerate(splitted_ip):
+            if index % 2 == 0:
+                self.non_hypernet_seq.append(block)
+            if index % 2 == 1:
+                self.hypernet_seq.append(block)
 
-    def has_ABBA_in_non_hypernet_sequences(self):
-        pass
+    def has_palindrome_in_non_hypernet_seq(self):
+        for sequence in self.non_hypernet_seq:
+            has_palindrome = self.has_palindrome_of_length_four_with_different_chars(sequence)
+            if has_palindrome:
+                return True
+        return False
 
-    def has_ABBA_in_hypernet_sequences(self):
-        pass
+    def has_palindrome_in_hypernet_seq(self):
+        for sequence in self.hypernet_seq:
+            has_palindrome = self.has_palindrome_of_length_four_with_different_chars(sequence)
+            if has_palindrome:
+                return True
+        return False
+
+    @staticmethod
+    def has_palindrome_of_length_four_with_different_chars(sequence: str) -> bool:
+        for i, char in enumerate(sequence):
+            try:
+                first = sequence[i]
+                second = sequence[i + 1]
+                third = sequence[i + 2]
+                fourth = sequence[i + 3]
+
+                if first == fourth and second == third and first != second:
+                    return True
+
+            except IndexError:
+                return False
+        return False
+
+
+class TLSCounter:
+    number_of_tls_ips: int
+
+    def __init__(self):
+        self.number_of_tls_ips = 0
+
+    def count_ips_with_tls(self, ips: list[str]):
+        for ip in ips:
+            checker = TLSChecker(ip)
+            if checker.supports_tls():
+                self.number_of_tls_ips += 1
+
+        return self.number_of_tls_ips
