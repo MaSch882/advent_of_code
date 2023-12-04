34b0573bbfb04d4223822d0685a0eac9922be98c
Matthias Schulte <matthias.schulte@adesso-insurance-solutions.de>
2023-12-04 14:21:39 +0100

    2023 - Problem 3
    Refactoring, Part 2 angefangen

--- 2023/03/Program.fs
+++ 2023/03/Program.fs
@@ -3,6 +3,7 @@ open System.IO
 
 let numbers = ["0";"1"; "2"; "3"; "4"; "5"; "6"; "7"; "8"; "9"]
 let delimiter = "."
+let gear = "*"
 
 let buildMatrixFromInput (filepath: string) =
      filepath 
@@ -10,17 +11,13 @@ let buildMatrixFromInput (filepath: string) =
      |> Seq.toList 
      |> List.map (fun line -> line.ToCharArray()) 
      |> Seq.toList
-    // for i in 0..lines.Length - 1 do 
-    //     for j in 0..9 do
-    //         printfn "%c " lines.[i].[j]
-    //     printfn "Ende Zeile"
 
-let isRelevant (s: string) =
+let isRelevantPartNumber (s: string) =
     (not (List.contains s numbers)) && s <> delimiter
 
 let checkEntry (i: int) (j: int) (matrix: char array list) =
     try
-        if isRelevant (string matrix.[i].[j]) then
+        if isRelevantPartNumber (string matrix.[i].[j]) then
             true
         else 
             false
@@ -65,16 +62,15 @@ let sumAllPartNumbers (matrix: char array list) =
         let mutable isRelevant = false
 
         for j in 0..matrix.[0].Length - 1 do
-            let currentChar = string(matrix.[i].[j])
-            // printfn "char = %s, i = %i, j = %i, relevant = %b" currentChar i j (matrix |> isRelevantNumber i j)
-            
-            let isCurrentCharANumber = List.contains currentChar numbers
+            let currentChar = string(matrix.[i].[j])            
 
             // Ist currentChar eine Zahl? Dann an currentNumber appenden.
+            let isCurrentCharANumber = List.contains currentChar numbers
             if isCurrentCharANumber then
                 currentNumber <- String.Concat([currentNumber; currentChar])
             
-            if isCurrentCharANumber && matrix |> isRelevantNumber i j && not (isRelevant) then 
+            // Falls gerade eine relevante Zahl am Zeiger ist, wird isRelevant auf true gesetzt.
+            if isCurrentCharANumber && matrix |> isRelevantNumber i j then
                 isRelevant <- true
 
             // Ist die Zahl zu Ende?
@@ -90,15 +86,42 @@ let sumAllPartNumbers (matrix: char array list) =
                         currentNumber <- ""
                         isRelevant <- false
             with 
+                // In diesem Fall ist in der if-Abfrage eine IndexOutOfRangeException geflogen.
+                // Dies ist äquivalent dazu, dass wir am rechten Rand der Matrix stehen.
+                // Daher tun wir hier nichts und pruefen diesen Fall einzeln ab.
                 _ -> ()
             
-            // Sind wir am rechten Rand der Matrix und unser letztes Zeichen ist eine Ziffer?
+            // Sind wir am rechten Rand der Matrix und unser letztes Zeichen ist eine Ziffer einer relevanten Zahl?
             if j = matrix.[0].Length - 1 && List.contains currentChar numbers && isRelevant then
                 sumOfPartNumbers <- sumOfPartNumbers + int(currentNumber)
                 currentNumber <- ""
                 isRelevant <- false
     sumOfPartNumbers
-                
+
+let isGear (i: int) (j: int) (matrix: char array list) =
+    if string matrix.[i].[j] <> gear then 
+        false 
+    else 
+        false
+
+
+let calculateGearRatio (matrix: char array list) (i: int) (j: int) = 
+    0
+
+let sumAllGearRatios (matrix: char array list) =
+    let mutable sumOfGearRatios = 0
+    
+    for i in 0..matrix.Length - 1 do 
+        for j in 0..matrix.[0].Length - 1 do 
+            if matrix |> isGear i j then 
+                printfn "Gear found at (%i, %i)" i j
+                let gearRatio = calculateGearRatio matrix i j
+                sumOfGearRatios <- sumOfGearRatios + gearRatio
+            else 
+                ()
+
+    sumOfGearRatios
+
 
 let fileIsGivenAndExists (arguments: String array) (filepath: string) = 
     arguments.Length = 1 && File.Exists filepath
@@ -116,7 +139,9 @@ let main argv =
         if filepath |> fileIsGivenAndExists argv then 
             printfn "Processing %s" filepath
             let part1 = filepath |> buildMatrixFromInput |> sumAllPartNumbers            
+            let part2 = filepath |> buildMatrixFromInput |> sumAllGearRatios
             printfn "Part 1: %i" part1
+            printfn "Part 2: %i" part2
             0
         else    
             printfn "File does not exist!"
