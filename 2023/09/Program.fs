9a1ae4271b78f16c1aaa8fac3d9d4299e7c36a9c
Matthias Schulte <matthias.schulte@adesso-insurance-solutions.de>
2023-12-11 15:48:12 +0100

    2023 - Problem 9
    Part 2

--- 2023/09/Program.fs
+++ 2023/09/Program.fs
@@ -42,8 +42,38 @@ let extrapolateNextNumber (numberList: list<int>) =
     
     sum
 
+
+let extrapolateBackwardNumber (numberList: list<int>) = 
+    let mutable currentList = numberList
+    let mutable firstNumbers = [numberList.[0]]
+    let mutable extrapolateSteps = new System.Collections.Generic.Dictionary<int, int>()
+
+    while not(currentList |> List.forall (fun element -> element = 0)) do 
+        let mutable nextList = []
+        
+        for i in 0..currentList.Length - 2 do
+            nextList <- nextList |> List.append [currentList.[i+1] - currentList.[i]]
+        nextList <- nextList |> List.rev    
+        
+        firstNumbers <- firstNumbers |> List.append [nextList.[0]]
+        currentList <- nextList
+
+    for i in 0..firstNumbers.Length - 1 do 
+        extrapolateSteps.Add(i, 0)
+    
+    firstNumbers <- firstNumbers |> List.rev
+
+    for i in firstNumbers.Length - 2..-1..0 do 
+        extrapolateSteps[i] <- firstNumbers.[i] - extrapolateSteps[i+1]
+
+    extrapolateSteps.[0]
+
+
 let sumAllExtrapolatedNumbers (numberLists: list<list<int>>) = 
     numberLists |> List.map (fun numberList -> numberList |> extrapolateNextNumber) |> List.sum
+
+let sumAllExtrapolatedBackwardNumbers (numberLists: list<list<int>>) = 
+    numberLists |> List.map (fun numberList -> numberList |> extrapolateBackwardNumber) |> List.sum
 
 let fileIsGivenAndExists (arguments: String array) (filepath: string) = 
     arguments.Length = 1 && File.Exists filepath
@@ -64,7 +94,7 @@ let main argv =
             let numberLists = filepath |> buildNumberLists
 
             let part1 = numberLists |> sumAllExtrapolatedNumbers
-            let part2 = -1
+            let part2 = numberLists |> sumAllExtrapolatedBackwardNumbers
             printfn "Part 1: %i" part1
             printfn "Part 2: %i" part2 
             0
