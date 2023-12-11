7e39f5fb58f2f802fe03bb8ffa1c3214d0b0c6b6
Matthias Schulte <matthias.schulte@adesso-insurance-solutions.de>
2023-12-11 15:53:20 +0100

    2023 - Problem 11
    Init

---
+++ 2023/11/Program.fs
@@ -0,0 +1,27 @@
+﻿open System
+open System.IO
+
+let fileIsGivenAndExists (arguments: String array) (filepath: string) = 
+    arguments.Length = 1 && File.Exists filepath
+
+[<EntryPoint>]
+let main argv = 
+    if argv.Length = 0 then 
+        printfn "Please specify a file!"
+        1
+    elif argv.Length > 1 then
+        printfn "More than one file given!"
+        2
+    else
+        let filepath = argv.[0]
+        if filepath |> fileIsGivenAndExists argv then 
+            printfn "Processing %s" filepath
+
+            let part1 = -1
+            let part2 = -1
+            printfn "Part 1: %i" part1
+            printfn "Part 2: %i" part2 
+            0
+        else    
+            printfn "File does not exist!"
+            2  
