b1cc72f1589f335e2905cb2730011306fe049c3c
Matthias Schulte <matthias.schulte@adesso-insurance-solutions.de>
2024-06-05 00:58:44 +0200

    Java
    2018-02, Part 2

--- loesungen/java/aoc/src/main/java/aoc2018/day02/Main.java
+++ loesungen/java/aoc/src/main/java/aoc2018/day02/Main.java
@@ -2,7 +2,6 @@ package aoc2018.day02;
 
 import aocUtils.InputReader;
 
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -14,10 +13,8 @@ public class Main {
         InputReader reader = new InputReader();
         List<String> lines = reader.readInput(filepath);
 
-        List<String> test = Arrays.asList("abcdef", "bababc", "abbcde", "abcccd", "aabcdd", "abcdee", "ababab");
-
         System.out.println("Part 1: " + calculateChecksum(lines));
-        System.out.println("Part 2: ");
+        System.out.println("Part 2: " + calculateCommonBoxLetters(lines));
     }
 
     public static int calculateChecksum(List<String> lines) {
@@ -25,19 +22,19 @@ public class Main {
         int numberStringsTriple = 0;
 
         for (String line : lines) {
-            Map<Character, Integer> characterMultiplicity = new HashMap<>();
+            Map<Character, Integer> charCount = new HashMap<>();
             for (char c : line.toCharArray()) {
-                if (characterMultiplicity.containsKey(c)) {
-                    characterMultiplicity.put(c, characterMultiplicity.get(c) + 1);
+                if (charCount.containsKey(c)) {
+                    charCount.put(c, charCount.get(c) + 1);
                 } else {
-                    characterMultiplicity.put(c, 1);
+                    charCount.put(c, 1);
                 }
             }
 
             boolean doubleFound = false;
             boolean tripleFound = false;
 
-            for (Map.Entry<Character, Integer> entry : characterMultiplicity.entrySet()) {
+            for (Map.Entry<Character, Integer> entry : charCount.entrySet()) {
                 if (entry.getValue() == 2) {
                     doubleFound = true;
                 }
@@ -55,5 +52,43 @@ public class Main {
         }
         return numberStringsDouble * numberStringsTriple;
     }
+
+    public static String calculateCommonBoxLetters(List<String> lines) {
+        for (int i = 0; i < lines.size(); i++) {
+            String currentLine = lines.get(i);
+            for (int j = i + 1; j < lines.size(); j++) {
+                String compareLine = lines.get(j);
+                int numberPosDiff = countNumberOfDifferentChars(currentLine, compareLine);
+
+                if (numberPosDiff == 1) {
+                    int position = getPositionOfDiffChar(currentLine, compareLine);
+                    return currentLine.substring(0, position) + currentLine.substring(position + 1);
+                }
+            }
+        }
+        throw new IllegalStateException("There is no common box letters in this line.");
+    }
+
+    private static int countNumberOfDifferentChars(String a, String b) {
+        int count = 0;
+        for (int i = 0; i < a.length(); i++) {
+            if (a.charAt(i) != b.charAt(i)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    private static int getPositionOfDiffChar(String currentLine, String compareLine) {
+        int position = -1;
+        for (int k = 0; k < currentLine.length(); k++) {
+            if (currentLine.charAt(k) != compareLine.charAt(k)) {
+                position = k;
+                break;
+            }
+        }
+        return position;
+    }
+
 
 }
