50207f13dae7dcb212e12647394850b72be2b2ff
Matthias Schulte <matthias.schulte@adesso-insurance-solutions.de>
2024-12-04 15:58:14 +0100

    2024, Problem 4
    Part 1

---
+++ loesungen/python/year_2024/day_04/problem_04.py
@@ -0,0 +1,145 @@
+from advent_of_code.loesungen.python.utils import input_processing as ip
+
+# INPUT
+
+def read_input(filename: str) -> list[str]:
+    input_reader = ip.InputReader()
+    return input_reader.read_input(filename)
+
+# PART 1
+
+def solve_part_1(lattice: list[str], word_to_search: str) -> int:
+    number_of_occurences: int = 0
+    # The number of occurences of the word to search in the lattice
+    # is the number of it ocurring forwards PLUS the number of it
+    # occuring backwards.
+    number_of_occurences += count_all_occurences(lattice, word_to_search)
+    number_of_occurences += count_all_occurences(lattice, word_to_search[::-1])
+
+    return number_of_occurences
+
+
+def count_all_occurences(lattice: list[str], word_to_search: str):
+    number_of_occurences: int = 0
+
+    number_of_occurences += count_all_horizontal_occurences(lattice, word_to_search)
+    number_of_occurences += count_all_vertical_occurences(lattice, word_to_search)
+    number_of_occurences += count_all_diagonal_downwards_occurences(lattice, word_to_search)
+    number_of_occurences += count_all_diagonal_upwards_occurences(lattice, word_to_search)
+
+    return number_of_occurences
+
+def count_all_horizontal_occurences(lattice: list[str], word_to_search: str) -> int:
+    if not lattice:
+        return 0
+
+    return sum([row.count(word_to_search) for row in lattice])
+
+def count_all_vertical_occurences(lattice: list[str], word_to_search: str) -> int:
+    if not lattice:
+        return 0
+
+    number_of_rows = len(lattice)
+    number_of_columns = len(lattice[0])
+
+    word_length = len(word_to_search)
+    if word_length > number_of_rows:
+        return 0
+
+    vertical_occurences = 0
+
+    # For each row...
+    for row_offset in range(number_of_rows):
+        # ...look through each column...
+        for column_number in range(number_of_columns):
+            word_found = ""
+            # ...exactly so far as we have length.
+            for row_number in range(word_length):
+                try:
+                    word_found += lattice[row_offset + row_number][column_number]
+                except IndexError:
+                    break
+            if word_found == word_to_search:
+                vertical_occurences += 1
+
+    return vertical_occurences
+
+def count_all_diagonal_downwards_occurences(lattice: list[str], word_to_search: str) -> int:
+    if not lattice:
+        return 0
+
+    number_of_rows = len(lattice)
+    number_of_columns = len(lattice[0])
+
+    word_length = len(word_to_search)
+    if word_length > number_of_rows:
+        return 0
+
+    diagonal_downwards_occurences = 0
+
+    # For each row...
+    for row_number in range(number_of_rows):
+        # ...and for each column...
+        for column_number in range(number_of_columns):
+            word_found = ""
+            # ...exactly so far as we have length.
+            for offset in range(word_length):
+                try:
+                    word_found += lattice[row_number + offset][column_number + offset]
+                except IndexError:
+                    break
+            if word_found == word_to_search:
+                diagonal_downwards_occurences += 1
+
+    return diagonal_downwards_occurences
+
+def count_all_diagonal_upwards_occurences(lattice: list[str], word_to_search: str) -> int:
+    if not lattice:
+        return 0
+
+    number_of_rows = len(lattice)
+    number_of_columns = len(lattice[0])
+
+    word_length = len(word_to_search)
+    if word_length > number_of_rows:
+        return 0
+
+    diagonal_upwards_occurences = 0
+
+    # For each row...
+    for row_number in range(number_of_rows):
+        # ...and for each column...
+        for column_number in range(number_of_columns):
+            word_found = ""
+            # ...exactly so far as we have length.
+            for offset in range(word_length):
+                try:
+                    # If index gets negative, we don't want to continue!
+                    if row_number - offset < 0:
+                        break
+                    word_found += lattice[row_number - offset][column_number + offset]
+                except IndexError:
+                    break
+            if word_found == word_to_search:
+                diagonal_upwards_occurences += 1
+
+    return diagonal_upwards_occurences
+
+# PART 2
+
+def solve_part_2(lattice):
+    pass
+
+
+def main():
+    filename = "C:/_Python/advent_of_code/advent_of_code/input/2024_04_real.txt"
+    lattice: list[str] = read_input(filename)
+
+    solution_part_1 = solve_part_1(lattice, "XMAS")
+    print(f"Part 1: There is {solution_part_1} times the word XMAS.")
+
+    # solution_part_2 = solve_part_2(lattice)
+    # print(f"Part 2: Sum of all valid multiplications is {solution_part_2}.")
+
+if __name__ == "__main__":
+    main()
---
+++ loesungen/python/year_2024/day_04/problem_04_test.py
@@ -0,0 +1,167 @@
+import unittest
+
+from advent_of_code.loesungen.python.year_2024.day_04.problem_04 import read_input, count_all_vertical_occurences
+from advent_of_code.loesungen.python.year_2024.day_04.problem_04 import count_all_occurences
+from advent_of_code.loesungen.python.year_2024.day_04.problem_04 import count_all_horizontal_occurences
+from advent_of_code.loesungen.python.year_2024.day_04.problem_04 import count_all_diagonal_downwards_occurences
+from advent_of_code.loesungen.python.year_2024.day_04.problem_04 import count_all_diagonal_upwards_occurences
+from advent_of_code.loesungen.python.year_2024.day_04.problem_04 import count_all_occurences
+from advent_of_code.loesungen.python.year_2024.day_04.problem_04 import solve_part_1
+
+
+test_lattice_1 = read_input("C:/_Python/advent_of_code/advent_of_code/input/2024_04_test_1.txt")
+test_lattice_2 = read_input("C:/_Python/advent_of_code/advent_of_code/input/2024_04_test_2.txt")
+
+
+class TestReadInput(unittest.TestCase):
+    def test_readTestInput(self):
+        actual = test_lattice_1
+        expected = ["..X...", ".SAMX.", ".A..A.", "XMAS.S", ".X...."]
+        self.assertEqual(actual, expected)
+
+class TestFindAllHorizontalOccurences(unittest.TestCase):
+    def test_emptyLattice(self):
+        actual = count_all_horizontal_occurences([], "XMAS")
+        expected = 0
+        self.assertEqual(actual, expected)
+
+    def test_oneRow_WithoutOccurence(self):
+        actual = count_all_horizontal_occurences(["..X..."], "XMAS")
+        expected = 0
+        self.assertEqual(actual, expected)
+
+    def test_oneRow_WithOccurence(self):
+        actual = count_all_horizontal_occurences(["XMAS.S"], "XMAS")
+        expected = 1
+        self.assertEqual(actual, expected)
+
+    def test_oneRow_WithTwoOccurences(self):
+        actual = count_all_horizontal_occurences(["XMASXMAS"], "XMAS")
+        expected = 2
+        self.assertEqual(actual, expected)
+
+    def test_twoRows_WithOccurencesEach(self):
+        actual = count_all_horizontal_occurences(["XMAS", "XMAS", "MASX"], "XMAS")
+        expected = 2
+        self.assertEqual(actual, expected)
+
+class TestFindAllVerticalOccurences(unittest.TestCase):
+    def test_emptyLattice(self):
+        actual = count_all_vertical_occurences([], "XMAS")
+        expected = 0
+        self.assertEqual(expected, actual)
+
+    def test_twoFewRows(self):
+        actual = count_all_vertical_occurences(["XXXX", "XXXX", "XXXX"], "XMAS")
+        expected = 0
+        self.assertEqual(expected, actual)
+
+    def test_enoughRowsWithOneOccurence_StartingInFirstRow(self):
+        actual = count_all_vertical_occurences(["XXXX", "MXXX", "AXXX", "SXXX"], "XMAS")
+        expected = 1
+        self.assertEqual(expected, actual)
+
+    def test_enoughRowsWithMultipleOccurences_StartingInFirstRow(self):
+        actual = count_all_vertical_occurences(["XXXX", "MMMM", "AAAA", "SSSS"], "XMAS")
+        expected = 4
+        self.assertEqual(expected, actual)
+
+    def test_enoughRowsWithOneOccurence_StartingInSecondRow(self):
+        actual = count_all_vertical_occurences(["JJJJ", "XXXX", "MXXX", "AXXX", "SXXX"], "XMAS")
+        expected = 1
+        self.assertEqual(expected, actual)
+
+    def test_enoughRowsWithMultipleOccurences_StartingInSecondRow(self):
+        actual = count_all_vertical_occurences(["JJJJ", "XXXX", "MMMM", "AADA", "SSSS"], "XMAS")
+        expected = 3
+        self.assertEqual(expected, actual)
+
+    def test_enoughRowsWithAllOccurences_StartingInSecondRow(self):
+        actual = count_all_vertical_occurences(["JJJJ", "XXXX", "MMMM", "AAAA", "SSSS"], "XMAS")
+        expected = 4
+        self.assertEqual(expected, actual)
+
+class TestFindAllDiagonalDownwardsOccurences(unittest.TestCase):
+    def test_emptyLattice(self):
+        actual = count_all_diagonal_downwards_occurences([], "XMAS")
+        expected = 0
+        self.assertEqual(expected, actual)
+
+    def test_enoughRowsWithOneOccurence_StartingInUpperLeft(self):
+        actual = count_all_diagonal_downwards_occurences(["XXXX", "XMXX", "XXAX", "XXXS"], "XMAS")
+        expected = 1
+        self.assertEqual(expected, actual)
+
+    def test_enoughRowsWithMultipleOccurence(self):
+        actual = count_all_diagonal_downwards_occurences(["X...X...", ".M...M..", ".XA...A.", "..MS...S", "...A....", "....S..."], "XMAS")
+        expected = 3
+        self.assertEqual(expected, actual)
+
+class TestFindAllDiagonalUpwardsOccurences(unittest.TestCase):
+    def test_emptyLattice(self):
+        actual = count_all_diagonal_upwards_occurences([], "XMAS")
+        expected = 0
+        self.assertEqual(expected, actual)
+
+    def test_enoughRowsWithOneOccurence_StartingInBottomLeft(self):
+        actual = count_all_diagonal_upwards_occurences(["XXXS", "XXAX", "XMXX", "XXXX"], "XMAS")
+        expected = 1
+        self.assertEqual(expected, actual)
+
+    def test_enoughRowsWithMultipleOccurence(self):
+        actual = count_all_diagonal_upwards_occurences(["......S", ".....A.", "...SM..", "..AX...", ".M.....", "X......"], "XMAS")
+        expected = 2
+        self.assertEqual(expected, actual)
+
+class TestTestLattice1(unittest.TestCase):
+    def test_testLattice1_horizontal(self):
+        actual = count_all_horizontal_occurences(test_lattice_1, "XMAS") + count_all_horizontal_occurences(test_lattice_1, "SAMX")
+        expected = 2
+        self.assertEqual(expected, actual)
+
+    def test_testLattice1_vertical(self):
+        actual = count_all_vertical_occurences(test_lattice_1, "XMAS") + count_all_vertical_occurences(test_lattice_1, "SAMX")
+        expected = 1
+        self.assertEqual(expected, actual)
+
+    def test_testLattice1_diagonal_downwards(self):
+        actual = count_all_diagonal_downwards_occurences(test_lattice_1, "XMAS") + count_all_diagonal_downwards_occurences(test_lattice_1, "SAMX")
+        expected = 1
+        self.assertEqual(expected, actual)
+
+    def test_testLattice1_diagonal_upwards(self):
+        actual = count_all_diagonal_upwards_occurences(test_lattice_1, "XMAS") + count_all_diagonal_upwards_occurences(test_lattice_1, "SAMX")
+        expected = 0
+        self.assertEqual(expected, actual)
+
+    def test_testLattice1_countAll_forward(self):
+        actual = count_all_occurences(test_lattice_1, "XMAS")
+        expected = 2
+        self.assertEqual(expected, actual)
+
+    def test_testLattice1_countAll_reversed(self):
+        actual = count_all_occurences(test_lattice_1, "SAMX")
+        expected = 2
+        self.assertEqual(expected, actual)
+
+    def test_testLattice1_solvePart1(self):
+        actual = solve_part_1(test_lattice_1, "XMAS")
+        expected = 4
+        self.assertEqual(expected, actual)
+
+class TestTestLattice2(unittest.TestCase):
+    def test_testLattice2_solvePart1(self):
+        actual = solve_part_1(test_lattice_2, "XMAS")
+        expected = 18
+        self.assertEqual(expected, actual)
+
+class TestEdgeCases(unittest.TestCase):
+    def test_multipleLines(self):
+        actual = solve_part_1(["XMXX","ASXX","XXXX","XXXX"], "XMAS")
+        expected = 0
+        self.assertEqual(expected, actual)
+
+    def test_overlap(self):
+        actual = solve_part_1(["XMASAMX"], "XMAS")
+        expected = 2
+        self.assertEqual(expected, actual)
